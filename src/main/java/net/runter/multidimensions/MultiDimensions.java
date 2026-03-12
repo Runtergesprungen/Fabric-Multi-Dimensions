a6cd90a7f3a690dcee4a7660a5d015281c675666
Timothé Philipp Schwabe <timothe.schwabe@gmx.de>
2026-03-12 18:11:34 +0100

    Idk

---
+++ src/main/java/net/runter/multidimensions/MultiDimensions.java
@@ -0,0 +1,16 @@
+package net.runter.multidimensions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MultiDimensions implements ModInitializer {
+	public static final String MOD_ID = "multidimensions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
